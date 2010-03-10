6a539b93024771e914cc70419b3c3a3979d837f3
joelabrahamsson <mail@joelabrahamsson.com>
2010-03-10 13:10:13 +0100

    Added separate file for EPiMvpPage<T> and added EPiMvpUserControl and EPiMvpUserControl<T>

--- EPiMVP/EPiMvpPage.cs
+++ EPiMVP/EPiMvpPage.cs
@@ -21,31 +21,4 @@ namespace EPiMVP
             base.OnInit(e);
         }
     }
-
-    /// <summary>
-    /// Represents a page that is a view with a strongly typed model in a Web Forms Model-View-Presenter application.
-    /// </summary>
-    public abstract class EPiView<TModel> : EPiMvpPage, IEPiView<TModel> where TModel : class, new()
-    {
-        TModel _model;
-
-        /// <summary>
-        /// Gets or sets the view model.
-        /// </summary>
-        /// <value>The view model.</value>
-        public TModel Model
-        {
-            get
-            {
-                if (_model == null)
-                    throw new InvalidOperationException("The Model property is currently null, however it should have been automatically initialized by the presenter. This most likely indicates that no presenter was bound to the control. Check your presenter bindings.");
-
-                return _model;
-            }
-            set
-            {
-                _model = value;
-            }
-        }
-    }
 }
---
+++ EPiMVP/EPiMvpUserControl`T.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EPiMVP
+{
+    public abstract class EPiMvpUserControl<TModel> : EPiMvpUserControl, IEPiView<TModel> where TModel : class, new()
+    {
+        TModel _model;
+
+        /// <summary>
+        /// Gets or sets the view model.
+        /// </summary>
+        /// <value>The view model.</value>
+        public TModel Model
+        {
+            get
+            {
+                if (_model == null)
+                    throw new InvalidOperationException("The Model property is currently null, however it should have been automatically initialized by the presenter. This most likely indicates that no presenter was bound to the control. Check your presenter bindings.");
+
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+    }
+}
