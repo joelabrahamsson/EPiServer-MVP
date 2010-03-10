ae4f31fea7f15e2688ddb6e6d317da2f00c864ca
joelabrahamsson <mail@joelabrahamsson.com>
2010-03-10 19:06:18 +0100

    Modified AssemblyInfo files for CTP1

--- EPiMVP/EPiPresenter.cs
+++ EPiMVP/EPiPresenter.cs
@@ -18,7 +18,6 @@ namespace EPiMVP
     /// <summary>
     /// This is a Presenter without a page type. 
     /// </summary>
-    /// <typeparam name="TView"></typeparam>
     public abstract class EPiPresenter<TView> : Presenter<TView> where TView : class, IView
     {
         protected EPiPresenter(TView view) : base(view) { }
